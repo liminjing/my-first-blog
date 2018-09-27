35eb9008d90e21b28b7575882fa578910bb8d3ea
maggy <114416663@qq.com>
2018-09-27 12:02:19 +0800

    add host agagin

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=q+&tstbcqmb4r--r^bftln^3^_ca7um*w=!_&%jo$#99))%r)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://maggy.pythonanywhere.com/',
+ALLOWED_HOSTS = ['maggy.pythonanywhere.com',
                  '127.0.0.1',
                  'localhost']
 
